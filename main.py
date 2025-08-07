e872f2857b1004d46609b442d63055d67c0e1c79
tiagoasazevedo <azevedosatiago@gmail.com>
2025-08-07 23:39:03 +0100

    add constants for screen dimensions and asteroid properties

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame
+import pygame # this allows us to use code from the open-source pygame library throughout this file
+import constants # this allows us to use constants defined in the constants.py file
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
