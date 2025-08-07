8dd28dc6e2423479d58ede6e231cf86372fcf186
tiagoasazevedo <azevedosatiago@gmail.com>
2025-08-07 02:10:36 +0100

    importing necessary stuf and creating an welcome message

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from asteroids!")
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
 
+def main():
+    print("Starting Asteroids!")
 
 if __name__ == "__main__":
     main()
