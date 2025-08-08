033113ea28d997ad82bef9f30b4443974b97d867
tiagoasazevedo <azevedosatiago@gmail.com>
2025-08-08 02:30:06 +0100

    refactor: fix indentation in triangle method of Player class

--- player.py
+++ player.py
@@ -8,9 +8,9 @@ class Player (CircleShape):
         rotation = 0  
     
     def triangle(self):
-    forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    a = self.position + forward * self.radius
-    b = self.position - forward * self.radius - right
-    c = self.position - forward * self.radius + right
-    return [a, b, c]
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
