c9a2b3b8f8fb222aa1a3fff93c200d03f6965ed2
tiagoasazevedo <azevedosatiago@gmail.com>
2025-08-08 23:27:26 +0100

    feat: add move method to Player class for player movement based on rotation

--- player.py
+++ player.py
@@ -31,4 +31,7 @@ class Player (CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-            
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
